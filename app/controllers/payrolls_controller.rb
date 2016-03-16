ab8fd5680854d55671d009fa307d122076d17e7a
Sergey Kutsko <kutsko86@gmail.com>
2016-03-16 16:37:28 +0200

    Basic specs and implementation

--- app/controllers/payrolls_controller.rb
+++ app/controllers/payrolls_controller.rb
@@ -7,6 +7,7 @@ class PayrollsController < ApplicationController
   end
 
   def create
+    Payroll.create
     redirect_to action: :index
   end
 
--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -1,3 +1,45 @@
 class Payroll < ActiveRecord::Base
+  STARTS = 5
+  ENDS = 20
+
   scope :ordered, -> { order(starts_at: :asc) }
+  validates :starts_at, :ends_at, presence: true
+
+  before_validation :set_payrolls_attributes, on: :create
+
+  private
+
+  def set_payrolls_attributes
+    raise "wrong days limints" unless valid_days_limit?
+    self.starts_at = starts
+    self.ends_at = ends
+  end
+
+  def valid_days_limit?
+    STARTS > 0 && STARTS <= 30 && ENDS > 1 && ENDS <= 31 && ENDS > STARTS
+  end
+
+  def starts
+    last_payroll ? get_starts_at : Time.now.change({ day: STARTS })
+  end
+
+  def ends
+    last_payroll ? get_ends_at : Time.now.change({ day: ENDS - 1 }).change({ day: ENDS - 1 })
+  end
+
+  def last_payroll
+    Payroll.ordered.last
+  end
+
+  def get_starts_at
+    last_payroll.ends_at.advance({ days: 1 })
+  end
+
+  def get_ends_at
+    if self.starts_at.day != STARTS
+      self.starts_at.advance({ months: 1 }).change({ day: STARTS - 1 })
+    else
+      self.starts_at.change({ day: ENDS - 1 })
+    end
+  end
 end
