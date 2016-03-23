6b12bbf513b428c434d13f5cf271efeaff2993bb
Mike Faraponov <java.math.random@bk.ru>
2016-03-23 12:18:20 +0200

    Separate last_payroll from make method to another.

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -8,27 +8,45 @@ class Payroll < ActiveRecord::Base
 
     if last_payroll = Payroll.all.last
       if last_payroll.ends_at.day.between?(FIRST_DATE, SECOND_DATE)
-        ends_at = FIRST_DATE - 1
+        ends_at_day = FIRST_DATE - 1
         ends_at_month = last_payroll.ends_at.next_month.month
-        ends_year = last_payroll.ends_at.next_month.year
+        ends_at_year = last_payroll.ends_at.next_month.year
       else
-        ends_at = SECOND_DATE - 1
+        ends_at_day = SECOND_DATE - 1
         ends_at_month = last_payroll.ends_at.month
-        ends_year = last_payroll.ends_at.year
+        ends_at_year = last_payroll.ends_at.year
       end
     else
       ends_at_month = 1
-      ends_year = Time.now.year
-      ends_at = SECOND_DATE - 1
+      ends_at_year = Time.now.year
+      ends_at_day = SECOND_DATE - 1
     end
 
+    starts_at = starts_at_from_payroll(last_payroll)
+
     Payroll.new(
-      starts_at: Date.new((last_payroll && last_payroll.ends_at.tomorrow.year) || Time.now.year,
-                          (last_payroll && last_payroll.ends_at.tomorrow.month) || 1,
-                          (last_payroll && last_payroll.ends_at.tomorrow.day) || FIRST_DATE),
-      ends_at: Date.new(ends_year, ends_at_month, ends_at)
+      starts_at: Date.new(starts_at[:year], starts_at[:month], starts_at[:day]),
+      ends_at: Date.new(ends_at_year, ends_at_month, ends_at_day)
     )
 
+  end
+
+  private
+
+  def self.starts_at_from_payroll(last_payroll)
+    if last_payroll
+      {
+        year: last_payroll.ends_at.tomorrow.year,
+        month: last_payroll.ends_at.tomorrow.month,
+        day: last_payroll.ends_at.tomorrow.day
+      }
+    else
+      {
+        year: Time.now.year,
+        month: 1,
+        day: FIRST_DATE
+      }
+    end
   end
 
 end
