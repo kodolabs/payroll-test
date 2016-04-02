9cb7e9da271a52abd8e9628d02099e4e708284b3
Aleksandr Kudashkin <al.kudashkin@gmail.com>
2016-04-02 18:20:06 +0300

    Two specs added.

--- spec/models/payroll_spec.rb
+++ spec/models/payroll_spec.rb
@@ -24,6 +24,16 @@ RSpec.describe Payroll, type: :model do
     it 'handles different years' do
       expect(payroll.starts_at..payroll.ends_at).to cover(today)
     end
+
+    it 'with starts_at = 20.12.2016' do
+      set_points(5, 20)
+      expect(payroll.starts_at).to eq(Date.new(2016, 12, 20))
+    end
+
+    it 'with ends_at = 4.01.2017' do
+      set_points(5, 20)
+      expect(payroll.ends_at).to eq(Date.new(2017, 1, 4))
+    end
   end
 
   describe 'It raise ArgumentError' do
