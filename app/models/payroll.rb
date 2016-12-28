bb2941c4a8edd4f31aea0120c88644075750a957
Your Name <you@example.com>
2016-12-28 22:53:30 +0300

    green payroll validation tests

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -1,4 +1,15 @@
 class Payroll < ActiveRecord::Base
   scope :ordered, -> { order(starts_at: :asc) }
 
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validate :starts_at_cant_be_after_ends_at
+
+  private
+
+  def starts_at_cant_be_after_ends_at
+    return if starts_at.nil?
+    return if ends_at.nil?
+    errors.add(:starts_at, 'can not be after ends_at') if starts_at > ends_at
+  end
 end
