752a8eb7fe29dc9497299be988b84050b655be57
Your Name <you@example.com>
2016-12-29 21:48:40 +0300

    make PayrollCreate.create_new_payroll private

--- app/core/services/payroll_create.rb
+++ app/core/services/payroll_create.rb
@@ -3,6 +3,7 @@ class Services::PayrollCreate
     create_new_payroll
   end
 
+  private
   def create_new_payroll
     starts_at = Payroll.last_ends_at + 1.day
     ends_at = Utils::Schedule.next_scheduled_date_exclusive(starts_at + 1.day)
--- test/services/payroll_create_service_test.rb
+++ test/services/payroll_create_service_test.rb
@@ -14,7 +14,7 @@ class PayrollCreateServiceTest < ActiveSupport::TestCase
   test 'consistency for first payroll' do
     Payroll.stubs(:last_ends_at).returns('1994-07-12'.to_datetime)
     Utils::Schedule.stubs(:schedule_days).returns([6, 28])
-    payroll = @service.create_new_payroll
+    payroll = @service.send(:create_new_payroll)
     assert_equal payroll.starts_at, '1994-07-13'.to_datetime
     assert_equal payroll.ends_at, '1994-07-27'.to_datetime
   end
@@ -22,8 +22,8 @@ class PayrollCreateServiceTest < ActiveSupport::TestCase
   test 'consistency for some payrolls' do
     Utils::Schedule.stubs(:schedule_days).returns([6, 28])
     payroll_1 = create(:payroll, starts_at: '1994-07-06'.to_datetime, ends_at: '1994-07-27')
-    payroll_2 = @service.create_new_payroll
-    payroll_3 = @service.create_new_payroll
+    payroll_2 = @service.send(:create_new_payroll)
+    payroll_3 = @service.send(:create_new_payroll)
 
     assert_equal payroll_2.starts_at, '1994-07-28'.to_datetime
     assert_equal payroll_2.ends_at, '1994-08-05'.to_datetime
@@ -35,8 +35,8 @@ class PayrollCreateServiceTest < ActiveSupport::TestCase
   test 'nearest dates consistency test' do
     Utils::Schedule.stubs(:schedule_days).returns([6, 7])
     payroll_1 = create(:payroll, starts_at: '1994-06-07'.to_datetime, ends_at: '1994-07-05')
-    payroll_2 = @service.create_new_payroll
-    payroll_3 = @service.create_new_payroll
+    payroll_2 = @service.send(:create_new_payroll)
+    payroll_3 = @service.send(:create_new_payroll)
 
     assert_equal payroll_2.starts_at, '1994-07-06'.to_datetime
     assert_equal payroll_2.ends_at, '1994-07-06'.to_datetime
@@ -48,14 +48,14 @@ class PayrollCreateServiceTest < ActiveSupport::TestCase
   test 'changes in schedule' do
     Utils::Schedule.stubs(:schedule_days).returns([6, 28])
     payroll_1 = create(:payroll, starts_at: '1994-07-06'.to_datetime, ends_at: '1994-07-27')
-    payroll_2 = @service.create_new_payroll
+    payroll_2 = @service.send(:create_new_payroll)
 
     assert_equal payroll_2.starts_at, '1994-07-28'.to_datetime
     assert_equal payroll_2.ends_at, '1994-08-05'.to_datetime
 
     Utils::Schedule.stubs(:schedule_days).returns([6, 14, 28])
-    payroll_3 = @service.create_new_payroll
-    payroll_4 = @service.create_new_payroll
+    payroll_3 = @service.send(:create_new_payroll)
+    payroll_4 = @service.send(:create_new_payroll)
 
     assert_equal payroll_3.starts_at, '1994-08-06'.to_datetime
     assert_equal payroll_3.ends_at, '1994-08-13'.to_datetime
